220eb422809cb6975c0538f89574c362a786b40e
arzzzae <r.johncandoy@gmail.com>
2016-06-24 23:46:47 -0500

    - Added a mini smoothscroll effect when you click the downward arrow button. - Improved Affix.js by making it check browser height dynamically.

--- PCCM.Web/App_Start/BundleConfig.cs
+++ PCCM.Web/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace PCCM.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/pccm.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap/bootstrap.css",
---
+++ PCCM.Web/Scripts/pccm.js
@@ -0,0 +1,24 @@
+﻿//          Affix
+//=================================
+$("#navbar").affix({
+    offset: {
+        top: $(".top-banner").height()
+    }
+});
+
+//      Mini Smoothscroll
+//=================================
+$(".top-banner .scrolldown").on('click', function (e) {
+
+    // prevent default anchor click behavior
+    e.preventDefault();
+
+    // animate
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 450, function () {
+
+        window.location.hash = this.hash;
+    });
+
+});
