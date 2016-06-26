6510b52f1371fa3f7d7f4e58e324abde54dcb358
arzzzae <r.johncandoy@gmail.com>
2016-06-25 20:43:22 -0500

    Added favicons for mobile devices and multiple browsers. Added working links for Resources, About and Media content.

---
+++ PCCM.Web/Controllers/MediaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCCM.Web.Controllers
+{
+    public class MediaController : Controller
+    {
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Vidoes()
+        {
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            return View();
+        }
+    }
+}
---
+++ PCCM.Web/Controllers/ResourcesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCCM.Web.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        // GET: Resources
+        public ActionResult AnnualReport()
+        {
+            return View();
+        }
+        
+        public ActionResult ConstitutionAndByLaws()
+        {
+            return View();
+        }
+
+        public ActionResult JobSearch()
+        {
+            return View();
+        }
+
+        public ActionResult Housing()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+
+        public ActionResult Legals()
+        {
+            return View();
+        }
+
+        public ActionResult UsefulLinks()
+        {
+            return View();
+        }
+    }
+}
